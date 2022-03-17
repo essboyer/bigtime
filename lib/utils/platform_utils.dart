628e51f5e4f849a46c5b7884aaefcdbe39dd5bb4
Sean Boyer <SeanBoyer@Tech-Office-iMac.local>
2022-03-17 14:13:25 -0300

    Clean up the plat utils

--- lib/utils/platform_utils.dart
+++ lib/utils/platform_utils.dart
@@ -1,15 +1,12 @@
 import 'dart:io';
-import 'dart:ui';
-
-import 'package:window_size/window_size.dart';
 
 class PlatformUtils {
-  // Is the device a desktop device?
+  /// Is the current device a desktop device?
   static bool isDesktop() {
     return Platform.isMacOS || Platform.isLinux || Platform.isWindows;
   }
 
-  // Is the device a mobile device?
+  /// Is the current device a mobile device?
   static bool isMobile() {
     return Platform.isAndroid || Platform.isIOS;
   }
