dae1ffb8a9ef1000bd16492c12f1115a404dbf2a
Sean Boyer <seanboyer.sj@gmail.com>
2022-03-21 00:06:27 -0300

    Things are shaping up

--- lib/screens/main_page/main_page_view_model.dart
+++ lib/screens/main_page/main_page_view_model.dart
@@ -1,10 +1,12 @@
+import 'package:bigtime/services/local_storage_service.dart';
 import 'package:flutter/material.dart';
 
 class MainPageViewModel extends ChangeNotifier {
-  bool advancedMode = true;
+  bool advancedMode = false;
   bool showSettings = false;
 
   void initialise() {
+    getSavedMode();
     notifyListeners();
   }
 
@@ -15,7 +17,16 @@ class MainPageViewModel extends ChangeNotifier {
 
   void onModeBtnPressed() {
     advancedMode = !advancedMode;
+    LocalStorageService.saveData('mode', advancedMode);
     notifyListeners();
   }
 
+  getSavedMode() async {
+    dynamic value = await LocalStorageService.readData('mode');
+
+    if (value is bool) {
+      advancedMode = value;
+      notifyListeners();
+    }
+  }
 }
